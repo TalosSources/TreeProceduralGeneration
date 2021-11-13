73a74d67292c0c6dd8e20a90f3973c2eec458ee2
Ismail Sahbane <ismail.sahbane@epfl.ch>
2021-11-13 17:03:12 +0100

    forest

--- Tree.java
+++ Tree.java
@@ -5,39 +5,34 @@ import java.io.IOException;
 import java.util.Random;
 
 public class Tree {
-    static Random RNG = new Random();
+	static Random RNG = new Random();
 
-    static Vector[] directions = {
-            new Vector().set(1, 0),
-            new Vector().set(-1, 0),
-            new Vector().set(-1,-1),
-            new Vector().set(0, -1),
-            new Vector().set(1, -1),
-    };
-
-    static BufferedImage colorSource = null;
-
-    static int MIN_BRANCH_LENGTH = 4;
-    static int MIN_BRANCH_LENGTH_2 = 12;
-    static double BRANCH_PROBABILITY = 0.01;
-
-    static int LEAF_SIZE = 60;
-    static int LEAF_DEPTH = 4;
 
     public static void main(String[] args) throws IOException {
-        BufferedImage image = new BufferedImage(1000, 1000, BufferedImage.TYPE_INT_ARGB);
-        colorSource = ImageIO.read(new File("cherryTreeColorSample.jpg"));
-        paintEntirely(image, 0xFFBFF7FF);
+		int dimension_x = 2000;
+		int dimension_y = dimension_x;
+        BufferedImage image = new BufferedImage(dimension_x, dimension_y, BufferedImage.TYPE_INT_ARGB);
+        paintEntirely(image, 0xFF193A70);
+		
+		BufferedImage sources[] = {
+			//ImageIO.read(new File("cherryTreeColorSample.jpg")),
+			ImageIO.read(new File("cheneColorSample.jpg")),
+			ImageIO.read(new File("sapinColorSample.jpg")),
+			ImageIO.read(new File("autumnColorSample1.jpg")),
+			ImageIO.read(new File("autumnColorSample2.jpg"))
+		};
 
-        drawTree(image);
+		int n = 25;
+		for(int i = 0; i < n; ++i) {
+			for(int j = 0; j < 3*n - 3*i; ++j){
+				Vector position = new Vector().set(RNG.nextInt(dimension_x), dimension_y * i / n + RNG.nextInt(dimension_y / n));		
+				TreeDrawer drawer = new TreeDrawer(image, sources[RNG.nextInt(sources.length)], position, dimension_x / 60 + i*dimension_x / (n * 40));		
+				drawer.drawTree();
+			}
+		}
+       
+		
         ImageIO.write(image, "png", new File("result.png"));
-    }
-
-    public static void drawTree(BufferedImage image) {
-        Vector position = new Vector().set(image.getWidth() / 2, image.getHeight() - 1);
-
-        branch(image, position, 3,250, 0);
-
     }
 
     public static void paintEntirely(BufferedImage image, int color){
@@ -47,75 +42,150 @@ public class Tree {
             }
         }
     }
+}
 
-    public static void branch(BufferedImage image,
-                              Vector pos,
-                              int dirIndex,
-                              int length,
-                              int depth) {
+class TreeDrawer {
+	static Random RNG = new Random();
 
-        if (length < MIN_BRANCH_LENGTH) return;
+    /*static Vector[] directions = {
+            new Vector().set(1, 0),
+            new Vector().set(-1, 0),
+            new Vector().set(-1,-1),
+            new Vector().set(0, -1),
+            new Vector().set(1, -1),
+    };*/
+	
+	static Vector[] directions = {
+            new Vector().set(1, 0),
+			new Vector().set(1, 1),
+			new Vector().set(0, 1),
+			new Vector().set(-1, 1),
+            new Vector().set(-1, 0),
+            new Vector().set(-1,-1),
+            new Vector().set(0, -1),
+            new Vector().set(1, -1),
+    };
+	
+	BufferedImage colorSource;
+	BufferedImage hostImage;
 
-        if(depth > LEAF_DEPTH) leaf(image, pos);
+    int MIN_BRANCH_LENGTH; //commented if must depend on tree size
+    int MIN_BRANCH_LENGTH_2; //
+    static double BRANCH_PROBABILITY = 0.004;
+
+    int LEAF_SIZE; // 180 for 500 first branch
+    static int LEAF_DEPTH = 2;
+	
+	static int a = 15;
+	static int b = -3*a;
+	
+	static int BRANCH_COLOR = 0xFFC9C8C8;
+	
+	int initLength;
+	Vector initPosition;
+	
+	//---------------CONSTRUCTORS--------------------------
+	public TreeDrawer(BufferedImage image, BufferedImage colorSource, Vector position, int length) {
+		this.colorSource = colorSource;
+		this.hostImage = image;
+		initLength = length;
+		
+		LEAF_SIZE = 2 *length / 5;
+		MIN_BRANCH_LENGTH = length / 40;
+		MIN_BRANCH_LENGTH_2 = length / 20;
+		
+		this.initPosition = position;
+		//drawTree();
+	}
+	
+	//---------------METHODS-------------------------------
+	
+	public void drawTree() {
+		//initially, 1st branch towards up, and depth is zero
+		branch(initPosition, 6, initLength, 0);
+    }
+	
+    void branch(Vector pos,
+                int dirIndex,
+                int length,
+                int depth) {
+
+        if (length < MIN_BRANCH_LENGTH || length == 0) {
+			leaf(pos);
+			return;
+		}			
 
         Vector position = new Vector().set(pos.x, pos.y);
-        int color = 0xFFC9C8C8;
+        int color = BRANCH_COLOR;
 
         for(int i = 0; i < length; ++i){
-            if(position.x < 0 || position.x >= image.getWidth() ||
-                    position.y < 0 || position.y >= image.getHeight()) return;
-            for(int k = 0; k < length / 10; ++k){
-                image.setRGB(position.x + k, position.y, color);
+            if(position.x < 0 || position.x >= hostImage.getWidth() ||
+                    position.y < 0 || position.y >= hostImage.getHeight()) return;
+			//if K is the range k below takes
+			//for the moment, K(L, i) = L/10   [doesn't depend on i]
+			//K(i) at the end for L must be equal to K(i) at the beginning for 2*L / 3
+			// K(L, i) = a * L + b * i (hypothesis that K is linear in L and i)
+			// K(L, L) = a * L + b * L = a * 2L/3 = K(2L/3, 0)
+			// => a * L / 3 + b * L = 0 => b = -a/3  ;  a is the width factor of a branch at it's beginning in function of it's length, set to 1/10
+			// so b = -1/30  => k ranges from -L/20 + i/60 to L/20 - i/60
+            for(int k = -length/a - i/b; k < length/a + i/b; ++k){
+				if(position.x + k < 0 || position.x + k >= hostImage.getWidth()) break;
+                hostImage.setRGB(position.x + k, position.y, color);
             }
             position.add(directions[dirIndex]);
             if(RNG.nextFloat() < BRANCH_PROBABILITY)
-                branch(image, position, addDir(dirIndex, RNG.nextInt(3) - 1), 2 * length / 3, depth + 1);
+                branch(position, addDir(dirIndex, RNG.nextInt(3) - 1), 2 * length / 3, depth + 1);
         }
+		
+		//if(depth > LEAF_DEPTH) leaf(image, position);
 
-        if (length < MIN_BRANCH_LENGTH_2) return;
+        if (length < MIN_BRANCH_LENGTH_2) {
+			leaf(position);
+			return;
+		}
 
-        branch(image, position, addDir(dirIndex, 1), 2 * length / 3, depth + 1);
-        branch(image, position, addDir(dirIndex, -1), 2 * length / 3, depth + 1);
-        branch(image, position, dirIndex, length / 2, depth + 1);
+        branch(position, addDir(dirIndex, 1), 2 * length / 3, depth + 1);
+        branch(position, addDir(dirIndex, -1), 2 * length / 3, depth + 1);
+        branch(position, dirIndex, length / 2, depth + 1);
     }
 
-    public static void leaf(BufferedImage image, Vector pos) {
+    void leaf(Vector pos) {
         int color = randomColor();
         for(int x = -LEAF_SIZE; x < LEAF_SIZE; ++x) {
             for(int y = (int)-Math.sqrt(LEAF_SIZE - x*x); y < (int)Math.sqrt(LEAF_SIZE - x*x); ++y) {
-                int x1 = pos.x + x - LEAF_SIZE / 2;
-                int y1 = pos.y + y - LEAF_SIZE / 2;
-                if(x1 < 0 || x1 >= image.getWidth() ||
-                        y1 < 0 || y1 >= image.getHeight()) return;
-                image.setRGB(x1, y1, color);
+                int x1 = pos.x + x;
+                int y1 = pos.y + y;
+                if(x1 < 0 || x1 >= hostImage.getWidth() ||
+                        y1 < 0 || y1 >= hostImage.getHeight()) return; //to put in an auxialiary method
+                hostImage.setRGB(x1, y1, color);
             }
         }
     }
-
-    static int addDir(int a, int b) {
-        return Math.floorMod((a + b), directions.length);
-    }
-
-    static int randomColor() {
+	
+	int randomColor() {
         int x = RNG.nextInt(colorSource.getWidth());
         int y = RNG.nextInt(colorSource.getHeight());
         return colorSource.getRGB(x, y);
     }
+	
+    static int addDir(int a, int b) {
+        return Math.floorMod((a + b), directions.length);
+    }
+}
 
-    static class Vector {
-        int x;
-        int y;
+class Vector {
+    int x;
+    int y;
 
-        Vector set(int x, int y) {
-            this.x = x;
-            this.y = y;
-            return this;
-        }
+    Vector set(int x, int y) {
+        this.x = x;
+        this.y = y;
+        return this;
+    }
 
-        Vector add(Vector other){
-            this.x += other.x;
-            this.y += other.y;
-            return this;
-        }
+    Vector add(Vector other){
+        this.x += other.x;
+        this.y += other.y;
+        return this;
     }
 }
